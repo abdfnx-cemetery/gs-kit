e723e8019af6b518306ae978d717ad535dfca2dc
abdfnx <abdfnabd@gmail.com>
2021-08-18 11:30:50 +0000

    define variables to `data/decode.go`

--- data/decode.go
+++ data/decode.go
@@ -9,3 +9,13 @@ import (
 	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
 )
+
+var (
+	typeDuration      = reflect.TypeOf(time.Duration(5))
+	typeTime          = reflect.TypeOf(time.Time{})
+	typeStringDecoder = reflect.TypeOf((*StringDecoder)(nil)).Elem()
+)
+
+type StringDecoder interface {
+	DecodeString(value string) error
+}
